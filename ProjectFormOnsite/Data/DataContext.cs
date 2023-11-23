3f69489a16d884bbf469c73123140e7db04b2cef
tuantv <tuantv@TUANTV>
2023-11-23 14:19:36 +0700

    Add project files.

---
+++ ProjectFormOnsite/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectFormOnsite.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opt) : base(opt)
+        {
+        }
+        #region DbSet
+       
+        #endregion
+    }
+}
