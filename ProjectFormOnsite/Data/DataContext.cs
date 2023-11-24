08548c5835a49fcb799aa7b1bc8474d0343aed9b
Truong Van Tuan <tuantv@gemvietnam.com>
2023-11-24 15:33:43 +0700

    fix-code

--- ProjectFormOnsite/Data/DataContext.cs
+++ ProjectFormOnsite/Data/DataContext.cs
@@ -12,24 +12,16 @@ namespace ProjectFormOnsite.Data
         public DbSet<Department> Departments { get; set; }
         public DbSet<Onsite> Onsites { get; set; }
         #endregion
-<<<<<<< HEAD
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Onsite>()
                 .HasOne(o => o.Employee)
                 .WithMany()
-=======
-        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Onsite>()
-                .HasOne(o => o.Employee)
-                .WithMany(e => e.OnsitesAsEmployee)
->>>>>>> d044544857ee7f92a98cede580844aeaa1d15e28
                 .HasForeignKey(o => o.EmployeeID);
 
             modelBuilder.Entity<Onsite>()
                 .HasOne(o => o.Approver)
-<<<<<<< HEAD
                 .WithMany()
                 .HasForeignKey(o => o.ApproverID);
             modelBuilder.Entity<Employee>()
@@ -37,14 +29,5 @@ namespace ProjectFormOnsite.Data
                 .WithMany()
                 .HasForeignKey(b => b.DepartmentID);
         }
-=======
-                .WithMany(e => e.OnsitesAsApprover)
-                .HasForeignKey(o => o.ApproverID);
-             modelBuilder.Entity<Employee>()
-                 .HasOne(b => b.Department)
-                 .WithMany()
-                 .HasForeignKey(b => b.DepartmentID);
-        }*/
->>>>>>> d044544857ee7f92a98cede580844aeaa1d15e28
     }
 }
--- ProjectFormOnsite/Data/Employee.cs
+++ ProjectFormOnsite/Data/Employee.cs
@@ -14,12 +14,7 @@ namespace ProjectFormOnsite.Data
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Address { get; set; }
-<<<<<<< HEAD
         public virtual Department? Department { get; set; }
-=======
-        /*public virtual ICollection<Onsite>? OnsitesAsEmployee { get; set; }
-        public virtual ICollection<Onsite>? OnsitesAsApprover { get; set; }
-        public virtual Department? Department { get; set; }*/
->>>>>>> d044544857ee7f92a98cede580844aeaa1d15e28
+
     }
 }
